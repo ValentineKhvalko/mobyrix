fbb055f615b6270d4ee2463c871bb53c39db1587
Valentine Khvalko <valentin.hvalko@whitesharx.com>
2023-06-23 15:21:14 +0300

    feat: update setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
+'use client';
+
 import React from 'react';
-import '../../styles/globals.css';
-import { reatomContext, setupBatch, withBatching } from '@reatom/npm-react';
-import { unstable_batchedUpdates } from 'react-dom';
+import { reatomContext } from '@reatom/npm-react';
 import { connectLogger, createCtx } from '@reatom/framework';
 
-setupBatch(unstable_batchedUpdates);
-const ctx = withBatching(createCtx());
+import '../../styles/globals.css';
+
+const ctx = createCtx();
 connectLogger(ctx);
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
